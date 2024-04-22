a8de77f3311598f6da547e544b5148ae377262ed
Kuenlun <kuenlun13054@gmail.com>
2024-04-22 23:44:33 +0200

    Added a basic SymEngine implementation

--- SymCiSo/src/main.cpp
+++ SymCiSo/src/main.cpp
@@ -1,10 +1,60 @@
 #include <iostream>
 #include <symengine/expression.h>
 
+namespace SymCiSo
+{
+
+	using SymEngine::Symbol;
+	using SymEngine::RCP;
+	using SymEngine::make_rcp;
+
+
+	class Impedance
+	{
+	public:
+		Impedance(const std::string& name)
+			: m_impedance{ make_rcp<const Symbol>(name) }
+		{
+		}
+
+		inline RCP<const Symbol> get_impedance() const { return m_impedance; }
+
+	private:
+		RCP<const Symbol> m_impedance;
+	};
+
+	class Resistor : private Impedance
+	{
+	public:
+		Resistor(const std::string& name) : Impedance(name) {}
+
+		inline RCP<const Symbol> get_resistance() const { return get_impedance(); }
+	};
+
+	class Capacitor : private Impedance
+	{
+	public:
+		Capacitor(const std::string& name) : Impedance(name) {}
+
+		inline RCP<const Symbol> get_capacitance() const { return get_impedance(); }
+	};
+
+	class Inductance : private Impedance
+	{
+	public:
+		Inductance(const std::string& name) : Impedance(name) {}
+
+		inline RCP<const Symbol> get_inductance() const { return get_impedance(); }
+	};
+
+} // namespace SymCiSo
+
+
 int main()
 {
-    using SymEngine::Expression;
-    Expression x("x");
-    auto ex = pow(x + sqrt(Expression(2)), 6);
-    std::cout << SymEngine::expand(ex) << std::endl;
+	SymCiSo::Resistor r1("R1");
+
+	const auto r1_resistance{ r1.get_resistance() };
+	const auto sum{ SymEngine::add(r1_resistance, SymEngine::number(1)) };
+	std::cout << SymEngine::Expression(sum) << std::endl;
 }
